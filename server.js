ba84dc535600307070d05fda7d15e2d2b1533938
GreenEyedOcelot <hbe123@gmail.com>
2018-09-22 09:49:50 -0400

    latest

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+
+var expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+var port = process.env.PORT || 8251;
+app.listen(port);
+
+
